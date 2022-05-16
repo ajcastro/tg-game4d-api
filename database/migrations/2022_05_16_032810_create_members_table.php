d9e8e1df764743ef2a4853ab3eb03c57d756ebf6
ajcastro <ajcastro29@gmail.com>
2022-05-16 11:35:59 +0800

    Add Member model and migration

---
+++ database/migrations/2022_05_16_032810_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('website_code')->index();
+            $table->string('website_username')->index()->comment('the original username of member in the website');
+            $table->string('username')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
